b6463b7039d425b732b95841b13f3d0251881101
Ovsienko Dmitry <ovsienkodima@mail.ru>
2024-04-14 18:55:57 +0300

    fix auto charset

--- src/api/fetchUrl.ts
+++ src/api/fetchUrl.ts
@@ -1,8 +1,18 @@
 export const fetchUrl = async (sourceRef: string) => {
   const data = await fetch(sourceRef || "");
+
+  const [, charset] = data.headers.get("Content-Type")?.split(";") || [];
+  const indexCharset = charset?.indexOf("=");
+  const currentCharset = charset
+    ?.slice(indexCharset + 1)
+    ?.trim()
+    .toLocaleLowerCase();
+
   const result = await data.arrayBuffer();
   const byteArray = new Uint8Array(result);
-  const decodedText = new TextDecoder("windows-1251").decode(byteArray);
+  const decodedText = new TextDecoder(currentCharset ?? "utf-8").decode(
+    byteArray
+  );
 
   return decodedText;
 };
